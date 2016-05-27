1bb3fd76943b1b6e8f02571a5fa19969524af6ec
koudaiii <cs006061@gmail.com>
2016-05-27 20:44:43 +0900

    Add cli

---
+++ lib/githuberkun/cli.rb
@@ -0,0 +1,13 @@
+module Githuberkun
+  class CLI < Thor
+    desc "list GITHUB_TOKEN", "List repositories"
+    option :github_token
+    def list
+      github = Githuberkun::Github.new(options[:github_token])
+
+      github.repositories.each do |repository|
+        puts repository
+      end
+    end
+  end
+end
