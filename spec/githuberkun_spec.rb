7a781253e050b31cf3945d209f105c1387dd5e16
koudaiii <cs006061@gmail.com>
2016-05-27 19:58:57 +0900

    Fix initial test

--- spec/githuberkun_spec.rb
+++ spec/githuberkun_spec.rb
@@ -4,8 +4,4 @@ describe Githuberkun do
   it 'has a version number' do
     expect(Githuberkun::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
